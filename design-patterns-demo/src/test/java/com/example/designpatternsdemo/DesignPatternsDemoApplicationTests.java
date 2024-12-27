09cf34a671e16472bda1915bfe3af40f407917ee
samad.mirzayev <samed.mirzayev@azericard.com>
2024-12-27 08:49:25 +0400

    empty project

---
+++ design-patterns-demo/src/test/java/com/example/designpatternsdemo/DesignPatternsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designpatternsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
